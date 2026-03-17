9db77ad49c008904c3d9ba81475bc1c47bf18ea1
MiishkaEugenioMNE <eugenio.mn@obmontessori.edu.ph>
2026-03-17 19:39:44 +0800

    Refactor electricity consumption classifier logic

--- Drill1.js
+++ Drill1.js
@@ -1,25 +1,25 @@
-//Electricity Consumption Rates
+//Electricity Consumption Classifier
 function classifier() {
-    let input = Number (document.getElementById("KWH_").value).toLowerCase()
+    let input = Number (document.getElementById("KWH_").value);
 
-switch (true) {
-    case (KWH_ <= 100):
-        document.getElementById("output").innerHTML = 'Lifeline Consumer: Discounted electricity rates';
-        break;
-    case (KWH_ > 101 && KWH_ <= 200):
-        document.getElementById("output").innerHTML = 'Low Consumption: Normal residential rate';
-        break;
-    case (KWH_ > 201 && KWH_ <= 300):
-        document.getElementById("output").innerHTML = 'Average Consumption: Typical electricity usage';
-        break;
-    case (KWH_ > 301 && KWH_ <= 500):
-        document.getElementById("output").innerHTML = 'High Consumption: Higher electricity usage';
-        break;
-    case (KWH_ > 501):
-        document.getElementById("output").innerHTML = 'Very High Consumption: Heavy electricity users';
-        break;
-    default:
-        document.getElementById("output").innerHTML = 'Invalid Output';
-        break;
-}
+    switch (true) {
+        case (input > 501):
+            document.getElementById("output").innerHTML = 'Very High Consumption: Heavy electricity users';
+            break;
+        case (input > 301):
+            document.getElementById("output").innerHTML = 'High Consumption: Higher electricity usage';
+            break;
+        case (input > 201):
+            document.getElementById("output").innerHTML = 'Average Consumption: Typical electricity usage';
+            break;
+        case (input > 100):
+            document.getElementById("output").innerHTML = 'Low Consumption: Normal residential rate';
+            break;
+        case (input >= 1):
+            document.getElementById("output").innerHTML = 'Lifeline Consumer: Discounted electricity rates';
+            break;
+        default:
+            document.getElementById("output").innerHTML = 'Invalid Output';
+            break;
+    }
 }
